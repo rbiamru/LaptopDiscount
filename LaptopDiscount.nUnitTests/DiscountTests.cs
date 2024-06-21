c385f811ef75f81931ad853f13cb96c33235b086
Amruthan Manjula Anandan <rbiamru@gmail.com>
2024-06-21 12:32:27 -0400

    Initial state of the project added test

---
+++ LaptopDiscount.nUnitTests/DiscountTests.cs
@@ -0,0 +1,16 @@
+namespace LaptopDiscount.nUnitTests
+{
+    public class DiscountTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
